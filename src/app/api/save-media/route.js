b1fa9abd9835401a0238d02339ea0a471a1edbad
Zoran Dašić <Zoran.Dasic@in.mtel.ba>
2025-10-21 17:13:20 +0200

    comment added

--- src/app/api/save-media/route.js
+++ src/app/api/save-media/route.js
@@ -1,8 +1,7 @@
 import clientPromise from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
-//CREATE NEW JSON OBJECT WITH GENERATED URL FROM CLODFLAER
-//AS A ATRIBUTE OF MONGDB COLLECTION
+//CREATE AND SAVE NEW JSON OBJECT WITH GENERATED URL FROM CLODFLAER
 export async function POST(req) {
   try {
     const data = await req.json();
